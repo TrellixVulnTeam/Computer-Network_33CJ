df7ccde90cbd48858a2d455e029c41f0f37c2373
Chonsawat Nakanam <61898331+chonsawat@users.noreply.github.com>
2021-04-09 20:39:29 +0700

    Prepare Option
    เตรียมทำ Client

--- Notebook/options.py
+++ Notebook/options.py
@@ -1,28 +1,33 @@
 
 from sys import exit
 
-
-def options():
-    while True:
-        print("""\nSelect options
+options_text = """
+Select options
 [M] Menu
 [C] Confirm
 [H] History
 [E] Exit
-""", end="")
+"""
+
+
+def options():
+    while True:
+        print(option_text, end="")
         option_ = input("Select option: ").upper()
         if option_ == 'M':
             print("Ying")
 
         elif option_ == 'C':
+            print("Ying")
             pass
         elif option_ == 'H':
+            print("Ying")
             pass
         elif option_ == 'E':
-            exit()
-            print("After")
+            print("Ying")
+            break
 
-    return "Options: "+option_
+    return "Options: " + option_
 
 
 options()
