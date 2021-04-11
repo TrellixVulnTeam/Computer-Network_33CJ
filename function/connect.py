be679b923117cf6ded09a033ab9c380190b11733
Chonsawat Nakanam <61898331+chonsawat@users.noreply.github.com>
2021-04-11 18:56:32 +0700

    แก้ไข Connect

--- function/connect.py
+++ function/connect.py
@@ -1,30 +1,37 @@
 from pymongo import MongoClient
-from pickle import load
-from function.member import line_break
-import os
+from member import line_break
+from IPython.display import clear_output
+from os import system
+from time import sleep
 
 
 def connect_mongo_db():
-    client = MongoClient("mongodb+srv://admin:admin@computernetwork.e5eod.mongodb.net/ComputerNetwork?authSource=admin&replicaSet=atlas-129xya-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true")
+    client = MongoClient("mongodb+srv://admin:admin@computernetwork.e5eod.mongodb.net/ComputerNetwork?authSource"
+                         "=admin&replicaSet=atlas-129xya-shard-0&w=majority&readPreference=primary&appname=MongoDB"
+                         "%20Compass&retryWrites=true&ssl=true")
     return client.ComputerNetwork
 
 
-def ask_port():
-    select = input("คุณต้องการเชื่อมต่อผ่าน Port พิเศษ? [Y]Yes / [Any]No: ")
-    if select.upper() == 'Y':
-        try:
-            while True:
-                port = check_port()
-                if port:
-                    break
-                line_break("ไม่พบเลข Port ดังกล่าว")
-            return int(port['port'])
-        except ValueError as v:
-            line_break("กรุณากรอกเฉพาะตัวเลขเท่านั้น เช่น 1234")
-            ask_port()
-    return 1234
+def ask_port(special=False):
+    if not special:
+        return 1234
+
+    try:
+        while True:
+            port = check_port(input("Enter special port: "))
+            if port:
+                break
+            line_break("ไม่พบเลข Port ดังกล่าว")
+            sleep(2)
+            _ = system('cls')
+            clear_output()
+        return int(port['port'])
+    except Exception as e:
+        print("Special:", e)
 
 
-def check_port():
+def check_port(input_data="1234"):
     db = connect_mongo_db()
-    return db.Port.find_one({"port": int(input("Enter special port: "))})
+    return db.Port.find_one({"port": input_data})
+
+print("Port:", ask_port(True))
