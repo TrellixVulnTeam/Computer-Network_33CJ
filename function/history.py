a2cc2c4b8706b38d508571f7949996772f947f8b
tongsunbun <69431177+tongsunbun@users.noreply.github.com>
2021-04-18 01:01:08 +0700

    code History

--- function/history.py
+++ function/history.py
@@ -1,6 +1,14 @@
 
-# Import
+import pymongo
+from function.member import line_break
+
+client = pymongo.MongoClient("mongodb+srv://admin:admin@computernetwork.e5eod.mongodb.net/ComputerNetwork?authSource=admin&replicaSet=atlas-129xya-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true")
+db = client.ComputerNetwork
 
 # Main
 def history(user=None):
-    pass
+    data = list(db.Request_logs.find({'usename': user}))
+    if not data:
+        line_break("Sorry,No history to show")
+    else:
+        print(data)
