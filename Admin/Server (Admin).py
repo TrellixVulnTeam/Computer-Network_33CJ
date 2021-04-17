a6541d015e705733c60712c83b1f8f9655f1b7ba
tongsunbun <69431177+tongsunbun@users.noreply.github.com>
2021-04-18 00:02:32 +0700

    test2

--- Admin/Server (Admin).py
+++ Admin/Server (Admin).py
@@ -45,36 +45,5 @@ while True:
 
 
 
-# print("\nWelcome to Chat Room\n")
-# print("Initialising....\n")
-# time.sleep(1)
-#
-#
-# port = 7777
-# s.bind((host, port))
-# print(host, "(", host, ")\n")
-# name = input(str("Enter your name: "))
-#
-# s.listen(1)
-# print("\nWaiting for incoming connections...\n")
-# conn, addr = s.accept()
-# print("Received connection from ", addr[0], "(", addr[1], ")\n")
-#
-# s_name = conn.recv(1024)
-# s_name = s_name.decode()
-# print(s_name, "has connected to the chat room\nEnter [e] to exit chat room\n")
-# conn.send(name.encode())
-#
-# while True:
-#     message = input(str("Me : "))
-#     if message == "[e]":
-#         message = "Left chat room!"
-#         conn.send(message.encode())
-#         print("\n")
-#         break
-#     conn.send(message.encode())
-#     message = conn.recv(1024)
-#     message = message.decode()
-#     print(s_name, ":", message)
 
 
